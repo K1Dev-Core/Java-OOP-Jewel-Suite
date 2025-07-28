f24b7a81009d1a876f02589be37e51c8b399eb30
Hexxxxxxx <67011212055@msu.ac.th>
2025-07-28 17:53:34 +0700

    Update Colors.java

--- src/Colors.java
+++ src/Colors.java
@@ -2,14 +2,14 @@ import java.awt.*;
 
 public class Colors {
 
-    public static final Color IOS_BLUE = new Color(0, 122, 255);
-    public static final Color IOS_GRAY = new Color(142, 142, 147);
-    public static final Color IOS_RED = new Color(255, 59, 48);
-    public static final Color IOS_ORANGE = new Color(255, 149, 0);
-    public static final Color IOS_YELLOW = new Color(255, 204, 0);
-    public static final Color IOS_GREEN = new Color(52, 199, 89);
-    public static final Color IOS_PURPLE = new Color(175, 82, 222);
-    public static final Color IOS_PINK = new Color(255, 45, 85);
+    public static final Color C_BLUE = new Color(0, 122, 255);
+    public static final Color C_GRAY = new Color(142, 142, 147);
+    public static final Color C_RED = new Color(255, 59, 48);
+    public static final Color C_ORANGE = new Color(255, 149, 0);
+    public static final Color C_YELLOW = new Color(255, 204, 0);
+    public static final Color C_GREEN = new Color(52, 199, 89);
+    public static final Color C_PURPLE = new Color(175, 82, 222);
+    public static final Color C_PINK = new Color(255, 45, 85);
 
     public static final Color BG_MAIN = new Color(248, 248, 248, 240);
     public static final Color BG_PANEL = new Color(255, 255, 255, 180);
@@ -22,15 +22,15 @@ public class Colors {
     public static final Color TEXT_DARK = new Color(28, 28, 30);
     public static final Color TEXT_LIGHT = new Color(99, 99, 102);
 
-    public static final Color RED = IOS_RED;
-    public static final Color YELLOW = IOS_YELLOW;
-    public static final Color GREEN = IOS_GREEN;
+    public static final Color RED = C_RED;
+    public static final Color YELLOW = C_YELLOW;
+    public static final Color GREEN = C_GREEN;
 
-    public static final Color BLUE = IOS_BLUE;
-    public static final Color SUCCESS = IOS_GREEN;
-    public static final Color SUCCESS_GREEN = IOS_GREEN;
-    public static final Color ORANGE = IOS_ORANGE;
-    public static final Color DANGER = IOS_RED;
-    public static final Color DANGER_RED = IOS_RED;
-    public static final Color PURPLE = IOS_PURPLE;
+    public static final Color BLUE = C_BLUE;
+    public static final Color SUCCESS = C_GREEN;
+    public static final Color SUCCESS_GREEN = C_GREEN;
+    public static final Color ORANGE = C_ORANGE;
+    public static final Color DANGER = C_RED;
+    public static final Color DANGER_RED = C_RED;
+    public static final Color PURPLE = C_PURPLE;
 }
